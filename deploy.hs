17c3aec5630d1847f352ca5cc884b6a9b3798b0f
Helge S. Holm <helge.holm@gmail.com>
2012-03-18 01:22:22 +0000

    Compatible with older versions of ghc.

--- deploy.hs
+++ deploy.hs
@@ -7,17 +7,20 @@ import List
 main = do
     hazNginx <- doesFileExist "/etc/init.d/nginx"
     hazNginxConf <- doesDirectoryExist "/etc/nginx/sites-enabled"
-    if not (hazNginx || hazNginxConf) then do
-        putStrLn "Could not find /etc/init.d/nginx - nginx not installed?"
-        fail "prerequisite failed: nginx"
-    else return ()
+    if not (hazNginx || hazNginxConf)
+        then do
+            putStrLn "Could not find /etc/init.d/nginx - nginx not installed?"
+            fail "prerequisite failed: nginx"
+        else
+            return ()
     -- yeah baby
     copyFile "nginx-plyply.com" "/etc/nginx/sites-enabled/plyply.com"
     removeDirectoryRecursive "/var/www-plyply/"
     _ <- readProcess "cp" ["-r", "www", "/var/www-plyply"] ""
     _ <- readProcess "/etc/init.d/nginx" ["reload"] ""
     nginxState <- readProcess "/etc/init.d/nginx" ["status"] ""
-    if "running" `elem` (words nginxState) then
-        return ()
-    else
-        putStrLn $ "Warning: nginx not running."
+    if "running" `elem` (words nginxState)
+        then
+            return ()
+        else
+            putStrLn $ "Warning: nginx not running."
