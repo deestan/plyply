8cc918ad8893c41f0291719e42e8f9c1829fed8a
Helge Skogly Holm <helge.holm@gmail.com>
2012-08-30 20:12:06 +0200

    Added beer lottery.

---
+++ www/beerlottery/lottery.js
@@ -0,0 +1,87 @@
+var arena = document.getElementById("arena");
+var playerInput = document.getElementById("players");
+var goButton = document.getElementById("run");
+var resetButton = document.getElementById("reset");
+
+var players = [];
+
+function createPlayers() {
+  function trim(x) {
+    return x.trim();
+  }
+  var playerNames = playerInput.innerText.split("\n").map(trim).filter(trim);
+  for (var i=0; i<players.length; i++) {
+    var pi = players[i].img;
+    pi.parentElement.removeChild(pi);
+    var nt = players[i].nameTag;
+    nt.parentElement.removeChild(nt);
+  }
+  players = [];
+  for (var i=0; i<playerNames.length; i++) {
+    var nameTag = document.createElement("span");
+    nameTag.classList.add("nametag");
+    nameTag.innerText = playerNames[i];
+    for (var j=0; j<100; j++) {
+      nameTag.innerText += " - " + playerNames[i];
+    }
+    nameTag.style.left = 0;
+    nameTag.style.top = players.length * 100 + 20;
+    arena.appendChild(nameTag);
+    var player = document.createElement("img");
+    player.src = "runner.gif";
+    player.classList.add("runner");
+    player.style.left = 0;
+    player.style.top = players.length * 100 + 10;
+    arena.appendChild(player);
+    players.push({img: player, nameTag: nameTag,
+                  left: 0, nextBound: 0, dx: 0});
+  }
+  arena.style.height = players.length * 100 + "px";
+}
+
+var running = null;
+function gogogo() {
+  playerInput.contentEditable = "false";
+  playerInput.onkeydown = null;
+  goButton.disabled = true;
+  running = setInterval(race, 80);
+}
+
+function win(player) {
+  clearInterval(running);
+  resetButton.disabled = false;
+  player.nameTag.style.color = "#fd3";
+}
+
+function race() {
+  var goal = arena.getBoundingClientRect().width - 70;
+  var winners = [];
+  for (var i=0; i<players.length; i++) {
+    var p = players[i];
+    if (p.left >= p.nextBound) {
+      p.dx = randy.best.triangular(3, 8, 5);
+      p.nextBound += 100;
+    }
+    p.left += p.dx;
+    p.img.style.left = p.left;
+    if (p.left >= goal) {
+      winners.push(p);
+    }
+  }
+  if (winners.length > 0) {
+    win(randy.best.choice(winners));
+  }
+}
+
+function reset() {
+  playerInput.contentEditable = "true";
+  playerInput.onkeydown = function () {
+    setTimeout(createPlayers, 0);
+  }
+  goButton.disabled = false;
+  createPlayers();
+}
+
+reset();
+goButton.onclick = gogogo;
+resetButton.onclick = reset;
