0207cd73dafd723aa9a3c6b6db4842dce337db09
Helge Skogly Holm <helge.holm@gmail.com>
2013-02-10 01:20:55 +0100

    Removed fading and fixed styling so it works in FF.

--- www/binaural/binaural.js
+++ www/binaural/binaural.js
@@ -3,11 +3,13 @@ var nextSound = null;
 var flipTimer = null;
 
 function start(el) {
+  if (el.classList.contains("disabled"))
+    return;
   toggleOff(el);
   if (flipTimer !== null)
     clearTimeout(flipTimer);
   if (currentSound !== null)
-    fade(currentSound, currentSound.volume, 0, 5);
+    currentSound.pause();
 
   var preId = "hz-" + el.dataset.beat + "-" + el.dataset.tone + "-";
   currentSound = document.getElementById(preId + "a");
@@ -15,53 +17,35 @@ function start(el) {
   currentSound.pause();
   nextSound.pause();
   currentSound.currentTime = 0.0;
-  fade(currentSound, 0, 1);
   currentSound.play();
-  flipTimer = window.setInterval(flip, 28000);
+  flipTimer = window.setInterval(flip, 29000);
 }
 
 function stop(el) {
+  if (el.classList.contains("disabled"))
+    return;
   toggleOff(el);
   if (flipTimer !== null) {
     clearTimeout(flipTimer);
     flipTimer = null;
   }
-  fade(currentSound, currentSound.volume, 0);
+  currentSound.pause();
 }
 
 function flip() {
   nextSound.currentTime = 0.0;
-  fade(nextSound, 0, 1);
   var _currentSound = currentSound;
-  window.setTimeout(function () {
-    fade(_currentSound, 1, 0);
-  }, 100);
   nextSound.play();
   var tmp = currentSound; currentSound = nextSound; nextSound = tmp;
 }
 
-function fade(snd, from, to, ticks) {
-  var tick = 0;
-  ticks = ticks || 10;
-  var timer = null;
-  snd.volume = from;
-  function tickDown() {
-    tick += 1;
-    if (tick >= ticks)
-      clearTimeout(timer);
-    var vol = from + (to - from) * (tick / ticks);
-    snd.volume = vol;
-  }
-  timer = window.setInterval(tickDown, 1000 / ticks);
-}
-
 function toggleOff(el) {
   var table = document.getElementById("buttonsTable");
-  var buttons = table.getElementsByTagName("button");
+  var buttons = table.getElementsByClassName("button");
   for (var i=0; i < buttons.length; i++) {
     var b = buttons[i];
     if (b !== el)
-      b.disabled = false;
+      b.classList.remove("disabled");
   }
-  el.disabled = true;
+  el.classList.add("disabled");
 }
